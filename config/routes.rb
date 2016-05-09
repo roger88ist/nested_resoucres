b77f6ad3a1adf7482e021fcb2c1054db2e51ecdc
Rogerio Istiraneopulos <roger88ist@gmail.com>
2016-05-09 14:28:11 -0400

    modified routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :departments
-  resources :organizations
+  # resources :organizations
+  resources :organizations do 
+    resources :departments
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
